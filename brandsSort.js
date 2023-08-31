999ce53cdf02895e4654d0a17228f572925e44a4
mezetu <lopory6982@gmail.com>
2023-08-31 13:51:54 +0300

    1.0

---
+++ brandsSort.js
@@ -0,0 +1,9 @@
+module.exports = brands = [
+    ['Обратно в меню'],
+    ['Aphex Twin - 5шт', 'MMY - 1шт'],
+    ['Drain Gang - 2шт', 'Thaiboy Digital - 1шт'],
+    ['MAISON MARGIELA - 1шт', 'Goth Money Records - 2шт'],
+    ['2Famous - 1шт', 'VETEMENTS - 1шт'],
+    ['Balenciaga - 1шт'],
+]
+
---
+++ index.js
@@ -0,0 +1,89 @@
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config()
+
+const token = '6481292373:AAFSsflpg5sAnfNYRimyXlE0LQKCsq5FYT4'
+const bot = new TelegramBot(token, {polling: true})
+const brand = require('./brandsSort')
+
+bot.setMyCommands( [
+    {command: '/start', description: 'Навигация'},
+    {command: '/menu', description: 'Вызвать меню'}
+])
+
+const bazeMenu =  { 
+    reply_markup: {
+        keyboard: [
+            ['Сортировка по бренду'],
+        ],
+        resize_keyboard: true
+    }
+}
+
+bot.on('message', async msg => {
+
+    const chatId = msg.chat.id
+
+    if(msg.text == '/start'){
+        await bot.sendMessage(chatId, 'Навигация по каналу: @internetlog')
+    }
+
+
+    if(msg.text === '/menu') {
+        await bot.sendMessage(chatId, `Меню бота`, bazeMenu)
+    }
+
+    if(msg.text == 'Сортировка по бренду') {
+        await bot.sendMessage(chatId, 'Бренды: ', { 
+            reply_markup: {
+                keyboard: brands,
+                resize_keyboard: true
+            }
+        })
+    }
+
+    if(msg.text == 'Aphex Twin - 5шт'){
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/66')
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/63')
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/29')
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/5')
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/3')
+    }
+
+    if(msg.text == 'MMY - 1шт'){
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/24')
+    }
+
+    if(msg.text == 'Drain Gang - 2шт'){
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/74')
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/38')
+    }
+
+    if(msg.text == 'Thaiboy Digital - 1шт'){
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/72')
+    }
+
+    if(msg.text == 'MAISON MARGIELA - 1шт'){
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/53')
+    }
+
+    if(msg.text == 'Goth Money Records - 2шт'){
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/70')
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/68')
+    }
+
+    if(msg.text == '2Famous - 1шт'){
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/78')
+    }
+
+    if(msg.text == 'VETEMENTS - 1шт'){
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/84')
+    }
+
+    if(msg.text == 'Balenciaga - 1шт'){
+        await bot.sendMessage(chatId, 'https://t.me/internetlog/92')
+    }
+
+    if(msg.text == 'Обратно в меню') {
+        await bot.sendMessage(chatId, `Меню бота`, bazeMenu)
+    }
+})
